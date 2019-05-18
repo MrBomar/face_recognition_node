ab883b1aa4d4aef1bc2e488ae0169caffa97db4c
MrBomar <mrlesbomar@gmail.com>
2019-05-18 16:52:41 -0500

    Error Correction

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res)=>res.send('it is working!'));
+app.get('/', (req, res) => res.json('it is working!'));
 app.post('/signin', (req, res) => SignIn.signIn(req, res, knex, bcrypt));
 app.post('/register', (req, res)=> Register.register(req, res, knex, bcrypt));
 app.get('/profile/:id', (req, res) => Profile.Profile(req, res, knex));
